fb338c543a5c9731bda42120a572745c01519115
hkhh972123 <hkhh972123@gmail.com>
2022-01-06 13:01:49 +0900

    Add Input message

---
+++ mission/src/components/Input.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="input">
+    <input v-model="message">
+    <button @click="submit(message)" name="submit" type="button">제출</button>
+    <button @click="alarm(message)" name="alarm" type="button">알림</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    submit(message) {
+      return message;
+    },
+    alarm(message) {
+      alert(message);
+    },
+  },
+};
+
+</script>
--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Input/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Input from '@/components/Input.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    Input,
   },
 };
+
 </script>
