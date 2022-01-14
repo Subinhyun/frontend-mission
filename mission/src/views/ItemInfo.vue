d9b4685ea9a05318a04d8b8c4f04abc930753542
hkhh972123 <hkhh972123@gmail.com>
2022-01-14 17:01:26 +0900

    Add main-image

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,5 +1,9 @@
 <template>
-<div id='item-info-page'></div>
+<div id='item-info-page'>
+  <div class="main">
+    <img id="main-Image" data-test="main-Image" :src="product.img">
+  </div>
+</div>
 </template>
 
 <script>
@@ -7,7 +11,14 @@ export default {
   name: 'ItemInfoPage',
   data() {
     return {
-
+      product: {
+        id: 0,
+        title: '슈퍼스타',
+        content: 'Born in France',
+        sale: 34,
+        price: 120000,
+        img: 'https://image.adidas.co.kr/upload/prod/basic/source/EH0050-01-01.jpg',
+      },
     };
   },
   methods: {
